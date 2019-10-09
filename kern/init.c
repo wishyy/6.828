ccf48f1aadb85ad1e1de34a47fa597616f57377a
wishy <likeaflyingcopper@gmail.com>
2019-10-09 13:58:43 +0800

    my solutions to lab 3

--- kern/init.c
+++ kern/init.c
@@ -18,15 +18,18 @@ void
 i386_init(void)
 {
 	extern char edata[], end[];
+	
 
 	// Before doing anything else, complete the ELF loading process.
 	// Clear the uninitialized global data (BSS) section of our program.
 	// This ensures that all static/global variables start out zero.
 	memset(edata, 0, end - edata);
+	
 
 	// Initialize the console.
 	// Can't call cprintf until after we do this!
 	cons_init();
+
 
 	cprintf("6828 decimal is %o octal!\n", 6828);
 
--- kern/pmap.c
+++ kern/pmap.c
@@ -157,6 +157,8 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+	envs = (struct Env *) boot_alloc(NENV * sizeof(struct Env));
+	memset(envs, 0, NENV * sizeof(struct Env));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -180,7 +182,7 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
-	boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U);
+	boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U | PTE_P);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map the 'envs' array read-only by the user at linear address UENVS
@@ -189,6 +191,7 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+	boot_map_region(kern_pgdir, UENVS, PTSIZE, PADDR(envs), PTE_U | PTE_P);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
@@ -287,8 +290,8 @@ page_init(void)
 		else
 		{
 			pages[i].pp_ref = 0;
-        		pages[i].pp_link = page_free_list;   //把后一个空闲页的pp_link指向前一个空闲页，所有的空闲页就链起来了
-        		page_free_list = &pages[i];
+        	pages[i].pp_link = page_free_list;   //把后一个空闲页的pp_link指向前一个空闲页，所有的空闲页就链起来了
+        	page_free_list = &pages[i];
 		}
 	}
 }
@@ -558,7 +561,20 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
+	
+	uint32_t start = ROUNDDOWN((uint32_t)va, PGSIZE);
+	uint32_t end = ROUNDUP((uint32_t)va + len, PGSIZE);
 
+	perm = perm | PTE_P;
+
+	for(int i = start; i < end; i+=PGSIZE) {
+		pte_t *pte_addr = pgdir_walk(env->env_pgdir, (const void *)i, 0);
+
+		if(i >= ULIM || !pte_addr || (*pte_addr & perm) != perm) {
+			user_mem_check_addr = i < (uint32_t)va ? (uintptr_t)va : i;
+			return -E_FAULT;
+		}
+	}
 	return 0;
 }
 
